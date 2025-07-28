be578d09a81ad59f7557d09a1b17e7077b0a0146
dariast03 <danteariastarifa@gmail.com>
2025-07-27 22:51:25 -0400

    feat: add DATABASE_URL to environment configuration and update schema validation

--- src/common/utils/envConfig.ts
+++ src/common/utils/envConfig.ts
@@ -12,9 +12,15 @@ const envSchema = z.object({
 
 	CORS_ORIGIN: z.string().url().default("http://localhost:8080"),
 
-	COMMON_RATE_LIMIT_MAX_REQUESTS: z.coerce.number().int().positive().default(1000),
+	COMMON_RATE_LIMIT_MAX_REQUESTS: z.coerce
+		.number()
+		.int()
+		.positive()
+		.default(1000),
 
 	COMMON_RATE_LIMIT_WINDOW_MS: z.coerce.number().int().positive().default(1000),
+
+	DATABASE_URL: z.string().min(10, "Invalid database URL"),
 });
 
 const parsedEnv = envSchema.safeParse(process.env);
