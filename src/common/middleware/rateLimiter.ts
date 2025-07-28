871d5686ad53a650a98b645e0120be2f3064ee4f
dariast03 <danteariastarifa@gmail.com>
2025-07-27 22:09:54 -0400

    refactor: update rate limiter and script commands for clarity

--- src/common/middleware/rateLimiter.ts
+++ src/common/middleware/rateLimiter.ts
@@ -1,15 +1,19 @@
-import type { Request } from "express";
-import { rateLimit } from "express-rate-limit";
+import type { Request } from 'express';
+import { rateLimit, ipKeyGenerator } from 'express-rate-limit';
 
-import { env } from "@/common/utils/envConfig";
+import { env } from '@/common/utils/envConfig';
 
 const rateLimiter = rateLimit({
-	legacyHeaders: true,
-	limit: env.COMMON_RATE_LIMIT_MAX_REQUESTS,
-	message: "Too many requests, please try again later.",
-	standardHeaders: true,
-	windowMs: 15 * 60 * env.COMMON_RATE_LIMIT_WINDOW_MS,
-	keyGenerator: (req: Request) => req.ip as string,
+  legacyHeaders: true,
+  limit: env.COMMON_RATE_LIMIT_MAX_REQUESTS,
+  message: 'Too many requests, please try again later.',
+  standardHeaders: true,
+  windowMs: 15 * 60 * env.COMMON_RATE_LIMIT_WINDOW_MS,
+  keyGenerator: (req, res) => {
+    if (req.query.apiKey) return req.query.apiKey as string;
+
+    return ipKeyGenerator(req.ip as string); // better
+  },
 });
 
 export default rateLimiter;
